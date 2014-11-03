ac0809a082be0fc087a54377af1c4955a5ac37ce
Mayank Mahajan <mayank.mahajan@gmail.com>
2014-11-03 03:25:29 -0500

    Added rest of files

---
+++ js/articles.js
@@ -0,0 +1,26 @@
+var articles = [
+  {
+    publication: "Columbia Spectrum",
+    date: "November 2, 2014",
+    title: "CU Raas to perform at the Met this afternoon",
+    link: "http://columbiaspectator.com/spectrum/2014/11/02/cu-raas-perform-met-afternoon"
+  },
+  {
+    publication: "Columbia Bwog",
+    date: "April 7, 2014",
+    title: "TamashaHop",
+    link: "http://bwog.com/2014/04/07/tamashahop/"
+  },
+  {
+    publication: "The Fordham Ram",
+    date: "April 5, 2014",
+    title: "WHY IT’S TIME TO CULTIVATE DIVERSITY’S ROOTS ON CAMPUS",
+    link: "http://fordhamram.com/2014/04/09/why-its-time-to-cultivate-diversitys-roots-on-campus/"
+  },
+  {
+    publication: "Columbia Daily Spectator",
+    date: "February 27, 2014",
+    title: "A guide to the various offerings at Lunar Gala ‘Midnight Trance’",
+    link: "http://columbiaspectator.com/arts-and-entertainment/2014/02/27/guide-various-offerings-lunar-gala-%E2%80%98midnight-trance"
+  }
+];
